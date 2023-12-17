ff2232843a32d2ae59c1adadcb6020a07c798e2a
Ajayi12 <timothyajayi58@gmail.com>
2023-12-17 06:57:27 -0800

    c program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,11 @@
- #include <stdio.h>
-
- /**
-  * main - program that print word with puts
-  *
-  * Return: Always 0 (success)
-  */
-
- int main(void)
- {
-    puts("\"Programming is like building a multilingual puzzle");
-
-    return (0);	    
- }    
+#include <stdio.h>
+/**
+ * main - program that print word with puts
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}    
