ce47cda140311d44b7676c5deb5e2a8fe44a7694
Ajayi12 <timothyajayi58@gmail.com>
2023-12-16 20:04:21 -0800

    hello world

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+ #include <stdio.h>
+
+ /**
+  * main - program that print word with puts
+  *
+  * Return: Always 0 (success)
+  */
+
+ int main(void)
+ {
+    puts("\"Programming is like building a multilingual puzzle");
+
+    return (0);	    
+ }    
