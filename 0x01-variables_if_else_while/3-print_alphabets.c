00268a190c2ace608e796f3b171e988170e2b404
Ajayi12 <timothyajayi58@gmail.com>
2023-12-19 16:39:33 -0800

    c program

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -4,20 +4,24 @@
  * main - program that prints the lower and upper case alphabet
  * Return: Always 0 (sucess)
  */
-int main()
+int main(void)
 {
 	char c;
 	char C;
 
 	c = 'a';
 	C = 'A';
-	while (c <= 'z' && C <= 'Z')
+	while (c <= 'z')
 	{
 		putchar(c);
-		putchar(C);
-		putchar(' ');
 		c++;
-		C++;
 	}
-	return(0);
+	while (C <= 'Z')
+	{
+		putchar(C);
+		putchar('\n');
+		C++;
+
+	}
+	return (0);
 }
