a59f7fb07802ebfd9a5ef64fda1714d4e37d38aa
Ajayi12 <timothyajayi58@gmail.com>
2023-12-26 15:54:59 -0800

    variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - program that prints the lowercase alphabet without q and e
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char c;
+
+	c = 'a';
+	while(c <= 'z')
+	{
+		if(c != 'e' && c != 'q')
+		{
+			putchar(c);
+		}
+		return (0);
+	}
+}
