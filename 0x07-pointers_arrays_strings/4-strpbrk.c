51aa203aabdb590bbdc3aff775f35e274ea1ad94
Ajayi12 <timothyajayi58@gmail.com>
2024-01-16 22:48:34 -0800

    more pointer

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -4,9 +4,11 @@
  * _strpbrk - function that searches for a string
  * @s: pointer to the character value of the parameter
  * @accept: pointer to the character value of the parameter
+ * Return: Always 0 (success)
  */
 char *_strpbrk(char *s, char *accept)
 {
 	char *result = strpbrk(s, accept);
+
 	return (result);
 }
