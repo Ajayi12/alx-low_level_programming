96bb67c3e36941aa6e82cca5c380a0c840580e55
Ajayi12 <timothyajayi58@gmail.com>
2024-01-27 09:20:02 -0800

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,17 +12,18 @@ char *str_concat(char *s1, char *s2)
 {
 	int i, result, result1, result3;
 	char *ptr;
-	
-	if (s1 == NULL && s2 == NULL)
-        {
+
+	if (s1 == NULL || s2 == NULL)
+	{
 		return (0);
-        }
+	}
 	result = strlen(s1);
 	result1 = strlen(s2);
 	result3 = result + result1;
-	ptr = (char*)malloc(result3 * sizeof(char));
+	ptr = (char *)malloc(result3 * sizeof(char));
 	if (ptr == NULL)
 	{
+		fprintf(stderr, "Error: failed to allocate memory\n");
 		return (NULL);
 	}
 	for (i = 0; i < result; i++)
