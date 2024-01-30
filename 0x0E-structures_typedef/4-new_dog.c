6a8e6507b35d35b182ce588f25435f2fbb3d6734
Ajayi12 <timothyajayi58@gmail.com>
2024-01-30 06:39:04 -0800

    struct

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+/**
+ * new_dog - function that create a new dog
+ * @name: parameters
+ * @age: parameters
+ * @owner: parameters
+ * Return: Always 0 (success)
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *new_one = malloc(sizeof(dog_t));
+	if (new_one == NULL)
+	{
+		return (NULL);
+	}
+	(*new_one).name = strdup(name);
+	(*new_one).age = age;
+	(*new_one).owner = strdup(owner);
+	if ((*new_one).name == NULL || (*new_one).owner == NULL)
+	{
+		return (NULL);
+	}
+	return (new_one);
+}
