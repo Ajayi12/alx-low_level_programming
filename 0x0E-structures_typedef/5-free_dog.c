c922ab474f052bcf8af0342c5e4ad522602bc63b
Ajayi12 <timothyajayi58@gmail.com>
2024-01-30 07:09:04 -0800

    struct

---
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dog.h"
+/**
+ * free_dog - function that frees dog
+ * @d: parameter
+ * Return: Always 0 (success)
+ */
+void free_dog(dog_t *d)
+{
+	free(d->name);
+	free(d->owner);
+	free(d);
+}
