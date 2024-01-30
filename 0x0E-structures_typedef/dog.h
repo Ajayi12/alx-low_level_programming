c46260e90368a9bd85c25817446b4e9eaa96c023
Ajayi12 <timothyajayi58@gmail.com>
2024-01-30 05:37:00 -0800

    struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,11 +1,17 @@
 #ifndef DOG_H
 #define DOG_H
+/**
+ * struct dog - structures that hold data type
+ * @name: member of the struct
+ * @age: member 2 of the struct
+ * @owner: member 3 of the struct
+ */
 typedef struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-}dog_t;
+} dog_t;
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 #endif /* DOG_H */
