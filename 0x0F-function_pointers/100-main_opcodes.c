5399451fa888e642e3555c1ffeef0390c78ac43d
Ajayi12 <timothyajayi58@gmail.com>
2024-02-02 07:21:38 -0800

    function pointer

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -24,7 +24,11 @@ int main(int argc, char **argv)
 	}
 	for (i = 0; i < number; i++)
 	{
-		printf("%02x ", ptr[i]);
+		printf("%02x", ptr[i]);
+		if (i != number - 1)
+		{
+			printf(" ");
+		}
 	}
 	printf("\n");
 	return (0);
