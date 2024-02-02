14d80847ffe9651e0ece6973dfbef756e6a07004
Ajayi12 <timothyajayi58@gmail.com>
2024-02-02 01:18:14 -0800

    function pointer

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -10,7 +10,6 @@
 int main(int argc, char *argv[])
 {
 	int a, b;
-	int (*operation)(int, int);
 
 	if (argc != 4)
 	{
@@ -23,18 +22,9 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-
-	operation = get_op_func(argv[2]);
-
-	if (operation == NULL)
-	{
-		printf("Error\n");
-		exit(99);
-	}
-
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	printf("%d\n", operation(a, b));
+	printf("%d\n", get_op_func(argv[2])(a, b));
 	return (0);
 }
